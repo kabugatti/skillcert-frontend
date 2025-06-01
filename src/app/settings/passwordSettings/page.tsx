180460b84874165c05178fd0b8bab7ed2cde25b1
chiscookeke11 <chiscookeke11@gmail.com>
2025-06-01 14:30:21 +0100

    fix

--- src/app/settings/passwordSettings/page.tsx
+++ src/app/settings/passwordSettings/page.tsx
@@ -1,5 +1,5 @@
 import ChangePasswordForm from "@/components/ChangePasswordForm";
-import SidebarSettings from "../components/sidebarSettings";
+
 
 
 
@@ -12,7 +12,6 @@ import SidebarSettings from "../components/sidebarSettings";
 export default function Page() {
     return (
         <div className="bg-[#111827] w-full flex items-start gap-10 h-screen px-8 py-6  "  >
-          <SidebarSettings/>
           <ChangePasswordForm/>
         </div>
     )
